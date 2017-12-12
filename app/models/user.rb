27611c746defad9d2fc2d72c7a1c32f7451fe6cc
svanhesteren <s.vanhesteren@gmail.com>
2017-12-12 22:15:48 +0100

    One-to_one association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :events, dependent: :destroy
+  has_one :profile
+
+  def full_name
+    return profile.full_name if profile?
+    email
+  end
+
+  def profile?
+    profile.present? && profile.persisted?
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171212201154) do
+ActiveRecord::Schema.define(version: 20171212210301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,16 @@ ActiveRecord::Schema.define(version: 20171212201154) do
     t.index ["theme_id", "event_id"], name: "index_events_themes_on_theme_id_and_event_id"
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.text "bio"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "themes", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -64,4 +74,5 @@ ActiveRecord::Schema.define(version: 20171212201154) do
   end
 
   add_foreign_key "events", "users"
+  add_foreign_key "profiles", "users"
 end
