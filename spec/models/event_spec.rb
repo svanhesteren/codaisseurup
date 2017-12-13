ed485626cead458d0937e1c36cf24e277d379721
svanhesteren <s.vanhesteren@gmail.com>
2017-12-13 22:41:10 +0100

    add model spec associations

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -66,23 +66,56 @@ RSpec.describe Event, type: :model do
       expect(exp_event.bargain?).to eq(false)
     end
 
-    describe ".order_by_price" do
-        event1 = Event.new(price: 200)
-        event2 = Event.new(price: 1500)
-        event3 = Event.new(price: 40)
-      # let!(:event1) { create :event, price: 100 }
-      # let!(:event2) { create :event, price: 200 }
-      # let!(:event3) { create :event, price: 50 }
-
-      it "returns a sorted array of rooms by prices" do
-        # note that they should not come out in the order that they were created
-        expect(Event.order_by_price).to eq([event3, event1, event2])
-      end
-    end
-    # it "returns sorted array of prices" do
-    #   expect(Event.order_by_price).to eq([event3, event1, event2])
-    #
-    # end
-
   end
+
+
+  describe ".order_by_price" do
+
+    #??????????
+    # event1 = Event.new(price: 200)
+    # event2 = Event.new(price: 1500)
+    # event3 = Event.new(price: 40)
+    let!(:event1) { create :event, price: 100 }
+    let!(:event2) { create :event, price: 200 }
+    let!(:event3) { create :event, price: 50 }
+
+    it "returns a sorted array of rooms by prices" do
+      # note that they should not come out in the order that they were created
+      expect(Event.order_by_price).to eq([event3, event1, event2])
+    end
+  end
+  # it "returns sorted array of prices" do
+  #   expect(Event.order_by_price).to eq([event3, event1, event2])
+  #
+  # end
+
+  describe "association with user" do
+    let(:user) { create :user }
+
+    it "belongs to a user" do
+      event = user.events.build(name: "Party yay")
+
+      expect(event.user).to eq(user)
+    end
+  end
+
+
+  describe "association with theme" do
+    let(:event) { create :event }
+
+    let(:theme1) { create :theme, name: "Bright", events: [event] }
+    let(:theme2) { create :theme, name: "Clean lines", events: [event] }
+    let(:theme3) { create :theme, name: "A Man's Touch", events: [event] }
+
+    it "has themes" do
+      expect(event.themes).to include(theme1)
+      expect(event.themes).to include(theme2)
+      expect(event.themes).to include(theme3)
+    end
+
+    # Or we do it the shoulda way:
+    # it { is_expected.to have_and_belong_to_many :themes }
+  end
+
+
 end
