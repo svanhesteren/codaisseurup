19a7ccb85bc948477690a57ff4ea909a26afe95a
svanhesteren <s.vanhesteren@gmail.com>
2017-12-14 17:35:15 +0100

    Create photo views

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   resources :events, except: [:destroy]
   resources :profiles, only: [:new, :edit, :create, :update]
+  resources :photos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
