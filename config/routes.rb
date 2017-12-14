c1411198a40402868265c7110ee4557f35aaa239
svanhesteren <s.vanhesteren@gmail.com>
2017-12-14 20:41:55 +0100

    Add about pages and tests

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :events, except: [:destroy]
   resources :profiles, only: [:new, :edit, :create, :update]
   resources :photos
+  get "about" => "pages#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
