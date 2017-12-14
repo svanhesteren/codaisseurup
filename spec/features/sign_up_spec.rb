52099b3129719cd9cb4409da0ac97ce522cd5bf1
svanhesteren <s.vanhesteren@gmail.com>
2017-12-14 21:06:51 +0100

    changed locale text back for some instances and sign up test

---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+describe "Signing up" do
+  before do
+    visit root_url
+    click_on "Sign Up"  #reminder Up is with a capital, not the others.
+    fill_in "user_email", with: "mail@codaisseur.com"
+  end
+
+  context "when password is too short" do
+    it "does not allow the user to sign up " do
+      fill_in "user_password", with: "short"
+      click_on "Sign up"
+      expect(page).to have_content "Password is too short"
+    end
+  end
+
+  context "when password is long enough" do
+    it "allowes the user to sign up" do
+      fill_in "user_password", with: "habahaba"
+      click_on "Sign up"
+      expect(page).to have_content "Welcome! You have signed up successfully."
+    end
+  end
+
+
+end
